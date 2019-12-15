1c37328a78934f945c084326de0009d188da3600
fankaikai <fankaikai@unity3d.com>
2019-12-15 15:01:51 +0800

    add SessionExpireFilter

---
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,35 @@
+package com.mmall.controller.common;
+
+import com.mmall.common.Const;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class SessionExpireFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String loginToken = CookieUtil.readLoginToken((HttpServletRequest) servletRequest);
+        if(!StringUtils.isEmpty(loginToken)){
+            String userJsonStr = RedisPoolUtil.get(loginToken);
+            if(!StringUtils.isEmpty(userJsonStr)){
+                RedisPoolUtil.expire(loginToken, Const.REDIS_SESSION_EXTIME);
+            }
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
